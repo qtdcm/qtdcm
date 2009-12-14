b3488bc03e1eae6f80ad482bd1b16760804777b3
abadie <abadie@6d9e27ec-2e28-0410-8ed1-dee9ee4ba72e>
2009-12-14 08:39:14 +0000

    QtDCM : Preferences dialog window can now load settings. Need now to implement add/remove PACS server feature.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/igns/trunk/QtWidgets/QtDcm@415 6d9e27ec-2e28-0410-8ed1-dee9ee4ba72e

--- Code/QtDcmPreferencesDialog.cpp
+++ Code/QtDcmPreferencesDialog.cpp
@@ -12,6 +12,11 @@ QtDcmPreferencesDialog::QtDcmPreferencesDialog(QWidget * parent)
     widget.setupUi(this);
     this->setModal(true);
     this->setParent(parent,Qt::Dialog);
+
+//    widget.treeWidget->setColumnWidth(0, 150);
+    widget.treeWidget->setColumnWidth(1, 100);
+    widget.treeWidget->setColumnWidth(2, 100);
+    widget.treeWidget->setColumnWidth(3, 150);
   }
 
 void QtDcmPreferencesDialog::initConnections()
@@ -25,12 +30,34 @@ QtDcmPreferencesDialog::setPreferences( QtDcmPreferences * prefs )
     _preferences = prefs;
     widget.localAetitleEdit->setText(_preferences->getAetitle());
     widget.localPortEdit->setText(_preferences->getPort());
+    for (int i = 0; i < _preferences->getServers().size(); i++)
+      {
+          QTreeWidgetItem * item = new QTreeWidgetItem(widget.treeWidget);
+          item->setText(0,_preferences->getServers().at(i)->getName());
+          item->setData(0,1,QVariant(_preferences->getServers().at(i)->getName()));
+          item->setText(1,_preferences->getServers().at(i)->getAetitle());
+          item->setData(1,1,QVariant(_preferences->getServers().at(i)->getAetitle()));
+          item->setText(2,_preferences->getServers().at(i)->getPort());
+          item->setData(2,1,QVariant(_preferences->getServers().at(i)->getPort()));
+          item->setText(3,_preferences->getServers().at(i)->getServer());
+          item->setData(3,1,QVariant(_preferences->getServers().at(i)->getServer()));
+      }
   }
 
 void QtDcmPreferencesDialog::updatePreferences()
   {
     _preferences->setAetitle(widget.localAetitleEdit->text());
     _preferences->setPort(widget.localPortEdit->text());
+
+    QTreeWidgetItem * root = widget.treeWidget->invisibleRootItem();
+    for (int i = 0 ; i < _preferences->getServers().size() ;i++)
+      {
+        _preferences->getServers().at(i)->setName(root->child(i)->data(0, 1).toString());
+        _preferences->getServers().at(i)->setServer(root->child(i)->data(1, 1).toString());
+        _preferences->getServers().at(i)->setAetitle(root->child(i)->data(2, 1).toString());
+        _preferences->getServers().at(i)->setPort(root->child(i)->data(3, 1).toString());
+      }
+
     _preferences->writeSettings();
   }
 
