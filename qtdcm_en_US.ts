1ddcafa6f752346b1997febd476bb4530df9ad19
rcarpent <rcarpent@6d9e27ec-2e28-0410-8ed1-dee9ee4ba72e>
2010-02-25 16:30:39 +0000

    Correction in CMake (so that QtNetwork can be compiled)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/igns/trunk/QtWidgets/QtDcm@484 6d9e27ec-2e28-0410-8ed1-dee9ee4ba72e

--- qtdcm_en_US.ts
+++ qtdcm_en_US.ts
@@ -1,283 +1,31 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0">
-<context>
-    <name>DateDialog</name>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmdatedialog.h" line="61"/>
-        <source>Date</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>PreferencesDialog</name>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="391"/>
-        <source>QtDcm settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="392"/>
-        <source>Binary paths</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="393"/>
-        <source>Dcm2nii path :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="394"/>
-        <source>Dcm4che path :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="395"/>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="396"/>
-        <source>Choose...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="397"/>
-        <source>Dicom settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="398"/>
-        <source>AE Title :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="399"/>
-        <source>TCP port :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="400"/>
-        <source>Hostname :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="401"/>
-        <source>Encoding :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="402"/>
-        <source>Local settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="403"/>
-        <source>Settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="405"/>
-        <source>Hostname</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="406"/>
-        <source>Port</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="407"/>
-        <source>AETitle</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="408"/>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="414"/>
-        <source>Name</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="409"/>
-        <source>Add</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="410"/>
-        <source>Remove</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="411"/>
-        <source>Server</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="412"/>
-        <source>AE Title</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="413"/>
-        <source>TCP port</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreferencesdialog.h" line="415"/>
-        <source>Servers settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>PreviewDialog</name>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreviewdialog.h" line="81"/>
-        <source>Preview</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcmpreviewdialog.h" line="82"/>
-        <source>Close</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
+<!DOCTYPE TS><TS>
 <context>
     <name>QtDCM</name>
     <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="266"/>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="267"/>
-        <source>Search</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="268"/>
-        <source>Name :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="269"/>
-        <source>Modality :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="272"/>
-        <source>MR</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="273"/>
-        <source>CT</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="274"/>
-        <source>US</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="275"/>
-        <source>PET</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="277"/>
-        <source>Advanced</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="278"/>
-        <source>Date :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="281"/>
-        <source>None</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="282"/>
-        <source>Aujourd&apos;hui</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="283"/>
-        <source>Hier</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="284"/>
-        <source>Date</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="285"/>
-        <source>Range</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="287"/>
-        <source>beginDate</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="288"/>
-        <source>-</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="289"/>
-        <source>endDate</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="290"/>
-        <source>Serie description :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="291"/>
-        <source>Study description :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../../builds/qtdcm2/ui_qtdcm.h" line="292"/>
-        <source>Patient ID :</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="Code/QtDcm.cpp" line="215"/>
         <source>Preview</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcm.cpp" line="223"/>
-        <location filename="Code/QtDcm.cpp" line="248"/>
         <source>Open dicomdir</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcm.cpp" line="226"/>
         <source>Query server</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcm.cpp" line="234"/>
         <source>Export</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcm.cpp" line="249"/>
         <source>Dicomdir (dicomdir DICOMDIR)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcm.cpp" line="276"/>
         <source>Export directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcm.cpp" line="296"/>
-        <location filename="Code/QtDcm.cpp" line="315"/>
         <source>Please select at least one serie to Export</source>
         <translation type="unfinished"></translation>
     </message>
@@ -285,52 +33,42 @@
 <context>
     <name>QtDcmManager</name>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="271"/>
         <source>Repertoire logs non cree</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="325"/>
-        <source>Probleme lors de la suppression du rÃ©pertoire temporaire</source>
+        <source>Probleme lors de la suppression du r&#xc3;&#xa9;pertoire temporaire</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="338"/>
         <source>Impossible de trouver dcm2nii, verifiez votre installation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="375"/>
         <source>Export termine avec succes !!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="382"/>
         <source>Getting images from CD...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="433"/>
         <source>Retrieving images from server...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="469"/>
         <source>Server query in progress...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="497"/>
         <source>Impossible de trouver dcmqr, verifiez votre installation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="555"/>
         <source>Aucune occurence pour cette recherche</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmManager.cpp" line="582"/>
         <source>Generating preview...</source>
         <translation type="unfinished"></translation>
     </message>
@@ -338,32 +76,26 @@
 <context>
     <name>QtDcmPreferencesDialog</name>
     <message>
-        <location filename="Code/QtDcmPreferencesDialog.cpp" line="170"/>
         <source>Dcm2nii (dcm2nii)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmPreferencesDialog.cpp" line="172"/>
         <source>Dcm2nii (dcm2nii.exe)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmPreferencesDialog.cpp" line="175"/>
         <source>Choose dcm2nii binary</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmPreferencesDialog.cpp" line="197"/>
         <source>Dcm4che (dcmqr)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmPreferencesDialog.cpp" line="199"/>
         <source>Dcm4che (dcmqr.bat)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Code/QtDcmPreferencesDialog.cpp" line="202"/>
         <source>Choose dcmqr binary</source>
         <translation type="unfinished"></translation>
     </message>
