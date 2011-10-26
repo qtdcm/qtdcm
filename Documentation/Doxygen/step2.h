81f9ff3bc5b067f3f2003f9cc5607a512a054295
Alexandre Abadie <Alexandre.Abadie@irisa.fr>
2011-10-26 11:50:46 +0200

    Update doxygen documentation

--- Documentation/Doxygen/step2.h
+++ Documentation/Doxygen/step2.h
@@ -1,11 +1,16 @@
 /**
  * \page step2page Configuring the project
  *
- * The project can be configured using CMake, see the <a href="http://www.cmake.org">CMake website</a> for more information.
+ * The project has to be configured using CMake, see the <a href="http://www.cmake.org">CMake website</a> for more information.
  *
  * \section dep_sec The dependencies
  *
  * \subsection qt_dep_sec Qt
+ *
+ * You can get the Qt development kit from <a href="http://qt.nokia.com/downloads/">the Qt website</a><br>
+ * On Ubuntu you can simply use the package manager: <a href="apt://libqt4-dev">libqt4-dev</a>
+ * <br><br>
+ * <code>sudo apt-get install libqt4-dev</code>
  *
  * \subsection dcmtk_dep_sec DCMTK
  *
