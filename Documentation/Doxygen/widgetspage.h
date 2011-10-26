bdc86a54c30eed079ff35faa7b6dcab2d89a84a4
Alexandre Abadie <Alexandre.Abadie@irisa.fr>
2011-10-27 00:58:25 +0200

    improve online documentation

--- Documentation/Doxygen/widgetspage.h
+++ Documentation/Doxygen/widgetspage.h
@@ -4,8 +4,10 @@
  * Here we describe the available widgets.
  * 
  * \section mainwidget_sec The main widget
+ * <img style="text-align:center;" src="http://qtdcm.gforge.inria.fr/qtdcm-main.png" width="500px">
  *
  * \section importwidget_sec The import widget
+ * <img style="text-align:center;" src="http://qtdcm.gforge.inria.fr/qtdcm-import.png" width="200px">
  *
  * \section previewwidget_sec The preview widget
  *
@@ -18,5 +20,6 @@
  * \section preferenceswidget_sec The global preferences widget
  * 
  * \section preferencesdialog_sec The global preferences dialog
+ * <img style="text-align:center;" src="http://qtdcm.gforge.inria.fr/qtdcm-settingsdialog.png" width="300px">
  *
  */
