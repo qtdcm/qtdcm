724872f05a20a41ac72ee54869296489e8c211d2
Alexandre Abadie <Alexandre.Abadie@irisa.fr>
2011-10-26 12:08:32 +0200

    Doxygen documentation improvement

--- Documentation/Doxygen/step1.h
+++ Documentation/Doxygen/step1.h
@@ -5,14 +5,11 @@
  * <ul>
  *  <li>Download the release archives from the <a href="https://gforge.inria.fr/frs/?group_id=3538">project
  *  download page on the INRIA GForge</a></li>
- *  <li>Getting the latest version of the code from the git repository:
- *  <br><br>
- *  <code type="bash">git clone git://scm.gforge.inria.fr/qtdcm/qtdcm.git</code>
- *
- *  </li>
+ *  <li>Getting the latest version of the code from the git repository:</li>
  * </ul>
+ *  <div id="code">$ git clone git://scm.gforge.inria.fr/qtdcm/qtdcm.git</div>
  *
  * <br><br>
  * 
- *  Now go to \ref step2page
+ *  Now go to \ref step2page step
  */
--- Documentation/Doxygen/step2.h
+++ Documentation/Doxygen/step2.h
@@ -1,5 +1,5 @@
 /**
- * \page step2page Configuring the project
+ * \page step2page Configuration
  *
  * The project has to be configured using CMake, see the <a href="http://www.cmake.org">CMake website</a> for more information.
  *
@@ -9,11 +9,28 @@
  *
  * You can get the Qt development kit from <a href="http://qt.nokia.com/downloads/">the Qt website</a><br>
  * On Ubuntu you can simply use the package manager: <a href="apt://libqt4-dev">libqt4-dev</a>
- * <br><br>
- * <code>sudo apt-get install libqt4-dev</code>
+ * <div id="code">
+ * $ sudo apt-get install libqt4-dev
+ * </div>
  *
  * \subsection dcmtk_dep_sec DCMTK
  *
+ * Download the source code from the <a href="ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk360/dcmtk-3.6.0.tar.gz">OFFIS website</a>
+ *
+ * On Linux, configure the code, compile and install using the following commands (from the source directory):
+ * <div id="code">
+ * $ CFLAGS="-fPIC" CXXFLAGS="-fPIC" AR="gcc" ARFLAGS="-shared -o" LIBEXT="so" RANLIB=":" ./configure<br>
+ * $ make && sudo make install-all
+ * </div>
+ *
  * \subsection itk_dep_sec The Insight Toolkit
+ *
+ * Download the 3.20 version of the Insight Toolkit from <a href="http://voxel.dl.sourceforge.net/sourceforge/itk/InsightToolkit-3.20.0.tar.gz">here</a>.
+ *
+ *
+ * \section config_sec Configuring QtDcm with CMake
+ *
+ *
+ * Go to the \ref step3page step
  * 
  */
