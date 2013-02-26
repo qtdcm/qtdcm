b94dd85858a9b8e366f99a32554fe101522ece15
Olivier Commowick <olivier.commowick@inria.fr>
2013-02-26 11:41:34 +0100

    Updating qtdcm to handle new finddcmtk on mac, fixing failed rpaths of dcmtk

---
+++ CMake/FixDCMTKMacLink.cmake
@@ -0,0 +1,23 @@
+MACRO(FixDCMTKMacLibLink lib_name)
+
+  foreach(lib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
+    add_custom_command(TARGET ${lib_name}
+	POST_BUILD
+	COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib lib/lib${lib_name}.dylib
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    )
+  endforeach()
+
+ENDMACRO(FixDCMTKMacLibLink)
+
+MACRO(FixDCMTKMacExeLink exe_name)
+
+  foreach(lib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
+    add_custom_command(TARGET ${exe_name}
+	POST_BUILD
+	COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib bin/${exe_name}
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    )
+  endforeach()
+
+ENDMACRO(FixDCMTKMacExeLink)
