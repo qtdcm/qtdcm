571cadc5f0e5afe0ba20cbd1b42b212b4a4b53c2
aabadie <alexandre.abadie@inria.fr>
2013-03-22 15:16:09 +0100

    Update step1.h

--- Documentation/Doxygen/step1.h
+++ Documentation/Doxygen/step1.h
@@ -7,7 +7,7 @@
  *  download page on the INRIA GForge</a></li>
  *  <li>Getting the latest version of the code from the git repository:</li>
  * </ul>
- *  <div id="code">$ git clone git://scm.gforge.inria.fr/qtdcm/qtdcm.git</div>
+ *  <div id="code">$ git clone git://github.com/aabadie/qtdcm.git</div>
  *
  * <br><br>
  * 
