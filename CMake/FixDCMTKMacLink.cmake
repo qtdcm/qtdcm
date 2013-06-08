e5d2faa2f6058054001951a3e91d99561af001c0
Olivier Commowick <olivier.commowick@inria.fr>
2013-06-08 15:45:49 +0200

    DCMTK links need fix on mac only if not built static

--- CMake/FixDCMTKMacLink.cmake
+++ CMake/FixDCMTKMacLink.cmake
@@ -1,11 +1,13 @@
 MACRO(FixDCMTKMacLibLink lib_name)
 
   foreach(lib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
-    add_custom_command(TARGET ${lib_name}
-	POST_BUILD
-	COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib lib/lib${lib_name}.dylib
-        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    )
+    if (EXISTS ${DCMTK_DIR}/lib/lib${lib}.dylib)
+      add_custom_command(TARGET ${lib_name}
+	    POST_BUILD
+	    COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib lib/lib${lib_name}.dylib
+          WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+      )
+    endif()
   endforeach()
 
 ENDMACRO(FixDCMTKMacLibLink)
@@ -13,11 +15,13 @@ ENDMACRO(FixDCMTKMacLibLink)
 MACRO(FixDCMTKMacExeLink exe_name)
 
   foreach(lib dcmdata dcmimage dcmimgle dcmjpeg dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 oflog ofstd)
-    add_custom_command(TARGET ${exe_name}
-	POST_BUILD
-	COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib ${exe_name}
-        WORKING_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}
-    )
+    if (EXISTS ${DCMTK_DIR}/lib/lib${lib}.dylib)
+      add_custom_command(TARGET ${exe_name}
+	    POST_BUILD
+	    COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change lib${lib}.dylib ${DCMTK_DIR}/lib/lib${lib}.dylib ${exe_name}
+          WORKING_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}
+      )
+    endif()
   endforeach()
 
 ENDMACRO(FixDCMTKMacExeLink)
