9b317ff6c9740b3046628e3389efebc07c8f25f3
Alexandre Abadie <alexandre.abadie@therapixel.com>
2015-10-09 09:53:01 +0200

    refactoring cmake

--- CMake/QtDCMSettings.cmake
+++ CMake/QtDCMSettings.cmake
@@ -1,16 +1,7 @@
-# SETTING installation directories
-set(INSTALL_CMAKE /lib/QtDCM-${QTDCM_VERSION_MAJOR}.${QTDCM_VERSION_MINOR}/)
-set(INSTALL_BIN /bin/)
-set(INSTALL_LIB /lib/)
-set(INSTALL_INC /include/qtdcm)
-set(INSTALL_DOC /doc/)
-set(INSTALL_MAN /man/)
-set(INSTALL_SHARE /share/)
-
 ## SETTING THE INCLUDE DIRS
 set(QTDCM_INCLUDE_DIRS
 ## Modules include dirs
-  ${PROJECT_SOURCE_DIR}/Code
+  ${PROJECT_SOURCE_DIR}/src
 )
 
 ## SETTING QTDCM LIBRARY DIRS
