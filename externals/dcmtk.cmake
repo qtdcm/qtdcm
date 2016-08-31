676648f77cba30c07899cb6fef7e902b9fbbf6f5
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-08-31 11:28:56 +0200

    cmake: settings external projects configuration

---
+++ externals/dcmtk.cmake
@@ -0,0 +1,31 @@
+message("${Esc}[1;32mConfiguring DCMTK external library")
+message("-------------------------${Esc}[m")
+
+if("${DCMTK_DIR}" STREQUAL "")
+  set(DCMTK_DIR ${PROJECT_BINARY_DIR}/externals/dcmtk-build)
+endif()
+if("${DCMTK_SOURCE_DIR}" STREQUAL "")
+  set(DCMTK_SOURCE_DIR ${PROJECT_BINARY_DIR}/externals/dcmtk)
+endif()
+set(DCMTK_SOURCE_DIR "${DCMTK_SOURCE_DIR}" CACHE PATH "" FORCE)
+set(DCMTK_DIR "${DCMTK_DIR}" CACHE PATH "" FORCE)
+
+ExternalProject_Add(
+  dcmtk 
+ 
+  GIT_REPOSITORY git://git.dcmtk.org/dcmtk.git
+  GIT_TAG DCMTK-3.6.1_20150924
+
+  CMAKE_GENERATOR Ninja
+  
+  SOURCE_DIR "${DCMTK_SOURCE_DIR}"
+  BINARY_DIR "${DCMTK_DIR}"
+  CMAKE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_APPS:BOOL=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDCMTK_WITH_ZLIB:BOOL=OFF 
+  
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+)
+
+message("${Esc}[1;32mDone")
+message("-------------------------${Esc}[m")
+message("")
---
+++ externals/itk.cmake
@@ -0,0 +1,26 @@
+message("${Esc}[1;32mConfiguring ITK thirdparty")
+message("--------------------------${Esc}[m")
+
+if("${ITK_DIR}" STREQUAL "")
+  set(ITK_DIR ${PROJECT_BINARY_DIR}/externals/itk-build)
+endif()
+  
+set(ITK_DIR "${ITK_DIR}" CACHE PATH "" FORCE)
+
+ExternalProject_Add(
+  itk
+ 
+  URL https://sourceforge.net/projects/itk/files/itk/4.10/InsightToolkit-4.10.0.tar.gz
+
+  CMAKE_GENERATOR Ninja
+  
+  BINARY_DIR "${ITK_DIR}"
+  CMAKE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo 
+  
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+)
+
+message("${Esc}[1;32mDone")
+message("-------------------------${Esc}[m")
+message("")
